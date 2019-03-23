665f9363b7e7e98ce29fe5b9e70ba69c910b05a3
Guttapercha <chitosan@inbox.ru>
2019-03-23 01:24:10 -0400

    Authentification added

--- config/config.php
+++ config/config.php
@@ -4,6 +4,14 @@ $baseConfig['env'] = 'production'; // 'development' or 'production'
 
 $baseConfig['appName'] = 'The LightMVC Framework Skeleton Application';
 
+$baseConfig['doctrine']['ORM']['dem1'] = [
+    'driver'   => 'pdo_mysql',
+    'host'     => 'localhost',
+    'user'     => 'lightmvc_user',
+    'password' => 'password',
+    'dbname'   => 'lightmvctestdb',
+];
+
 require 'routes.config.php';
 
 require 'view.config.php';
